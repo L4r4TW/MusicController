d76e09d855dfb43913b41f336f056f02fa0f8072
Bruno <bruno.banoczi@gmail.com>
2023-03-07 21:59:10 +0100

    remove commented junk code from views

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -11,31 +11,6 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
-# class CreateRoomView(APIView):
-#     serializer_class = CreateRoomSerializer
-
-#     def post(self, request, format = None):
-#         if not self.request.session.exist(self.request.session.session_key):
-#             self.request.session.create()
-
-#             serializer = self.serializer_class(data = request.data)
-#             if serializer.is_valid():
-#                 guest_can_pause = serializer.data.get('guest_can_pause')
-#                 votes_to_skip = serializer.data.get('votes_to_skip')
-#                 host = self.request.session.session_key
-#                 queryset = Room.objects.filter(host=host)
-#                 if queryset.exist():
-#                     room = queryset[0]
-#                     room.guest_can_pause = guest_can_pause
-#                     room.votes_to_skip = votes_to_skip
-#                     room.save(update_fields = ['guest_can_pause', 'votes_to_skip'])
-#                 else:
-#                     room = Room(host=host, guest_can_pause = guest_can_pause, votes_to_skip = votes_to_skip)
-#                     room.save()
-
-#                 return(RoomSerializer(room).data)
-
-
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
